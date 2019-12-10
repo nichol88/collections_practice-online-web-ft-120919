3fe74158551885ced4492268940578c08dea975d
Brian Nicholls <nichol88@gmail.com>
2019-12-10 21:26:33 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -50,7 +50,27 @@ def sort_array_desc(a)
 end
 
 def sort_array_char_count
+  r = []
+  i = 0
+  a.each{ |num|
+    if r.length == 0
+      r.push(num)
+    else
+      # compare to nums in r and see where it fits (< or >)
+      r.each_with_index{ |rnum, idx|
+        if num.length < rnum.length
+          r.unshift(num)
+          break
+        elsif idx == r.length - 1
+          r.push(num)
+          break
+        else
+        end
 
+      }
+    end
+  }
+  r
 end
 
 def swap_elements
