44e9a8302dfccfd743984d46602fff07a1db11e8
Brian Nicholls <nichol88@gmail.com>
2019-12-10 17:20:50 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -23,7 +23,7 @@ def sort_array_asc(a)
   r
 end
 
-def sort_array_desc(a)
+def sort_array_desc
   r = []
   i = 0
   a.each{ |num|
@@ -32,8 +32,8 @@ def sort_array_desc(a)
     else
       # compare to nums in r and see where it fits (< or >)
       r.each_with_index{ |rnum, idx|
-        if num < rnum
-          r.push(num)
+        if num > rnum
+          r.unshift(num)
           break
         elsif idx == r.length - 1
           r.unshift(num)
@@ -44,7 +44,6 @@ def sort_array_desc(a)
       }
     end
   }
-
   r
 end
 
